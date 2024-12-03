d14df81c614dc5d1734f30a26a40b89ef0a9d8c7
Victor CG <ubicuosoft@gmail.com>
2024-12-02 23:10:41 -0400

    while structure

--- src/controlstructures/WhileMain.kt
+++ src/controlstructures/WhileMain.kt
@@ -1,2 +1,21 @@
 package controlstructures
 
+fun main() {
+    //while
+    val greetings = arrayOf("Hola", "Hi", "Bienvenido", "Buenos dias", "Hallo")
+    var i=0
+    println("---------------")
+    println("Con while")
+    while(i <= greetings.size-1){
+        println(greetings[i])
+        i++
+    }
+
+    println("---------------")
+    println("Con dowhile")
+    var j=0
+    do {
+        println(greetings[j])
+        j++
+    } while (j <= greetings.size-1)
+}
