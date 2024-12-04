aaa7c0ce4a00e71e837a788ff431252ae7683215
Victor CG <ubicuosoft@gmail.com>
2024-12-04 00:57:45 -0400

    foreach

--- src/controlstructures/ForeachMain.kt
+++ src/controlstructures/ForeachMain.kt
@@ -1,2 +1,35 @@
 package controlstructures
 
+import kotlin.system.measureNanoTime
+
+fun main() {
+
+    val forInRange = measureNanoTime {
+        for (i in 0..1000) {
+            println(i)
+        }
+    }
+
+    val forEachRange = measureNanoTime {
+        (0..1000).forEach {
+            println(it)
+        }
+    }
+
+    val numberList = (0..1000).toList()
+
+    val forNumberList = measureNanoTime {
+        for (number in numberList){
+            println(number)
+        }
+    }
+
+    val numberListforEach= measureNanoTime {
+        numberList.forEach{println(it)}
+    }
+
+    println("El tiempo para forInRange es: $forInRange")
+    println("El tiempo para forEachRange es: $forEachRange")
+    println("El tiempo para forNumberList es: $forNumberList")
+    println("El tiempo para forNumberListforEach es: $numberListforEach")
+}
