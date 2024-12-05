98a98962cdfdfe40f0103252955b186d5622b1eb
Victor CG <ubicuosoft@gmail.com>
2024-12-04 21:24:58 -0400

    algorithm 1

--- src/algorithm/Main.kt
+++ src/algorithm/Main.kt
@@ -22,6 +22,34 @@ fun main() {
             numbers[index] = 0
         }
     }
+
+    println("---Solucion v3---")
+    if (numbers.size > 1) {
+        var currentIndex = 1
+        for (index in 1 until numbers.size) {
+            currentIndex = compareValuesAndGetTheCurrentIndex(index, numbers, currentIndex)
+        }
+
+        for (index in currentIndex until numbers.size) {
+            fillWithZeros(numbers, index)
+        }
+    }
+
     println("--Array con datos distintos---")
     println(numbers.toList())
+
+}
+
+private fun compareValuesAndGetTheCurrentIndex(index: Int, numbers: IntArray, currentIndex: Int): Int {
+    var currentIndex1 = currentIndex
+    var previusIndex = index - 1
+    if (numbers[previusIndex] != numbers[index]) {
+        numbers[currentIndex1] = numbers[index]
+        currentIndex1++
+    }
+    return currentIndex1
+}
+
+private fun fillWithZeros(numbers: IntArray, index: Int) {
+    numbers[index] = 0
 }
