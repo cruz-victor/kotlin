d946b39ed8e84ad51dec1a80189c23e862a45a0c
Victor CG <ubicuosoft@gmail.com>
2024-12-06 15:26:59 -0400

    hight order function

--- src/functions/LambdaMain.kt
+++ src/functions/LambdaMain.kt
@@ -1,2 +1,12 @@
 package functions
 
+fun main(){
+    val greating={"Hola Vix"}
+    println(greating())
+
+    val greatingWithName={text:String-> println(text)}
+    greatingWithName("Hola Vix Crux Gomex")
+
+    val add={x:Int->x+10}
+    println(add(5))
+}
