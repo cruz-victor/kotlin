bef1eb46a155f2541e35098a2d5cb184a54be65a
Victor CG <ubicuosoft@gmail.com>
2024-12-06 15:49:32 -0400

    hight order function

--- src/functions/OrdenSuperiorMain.kt
+++ src/functions/OrdenSuperiorMain.kt
@@ -1,2 +1,23 @@
 package functions
 
+fun main() {
+    println("---7 es mayor que 5---")
+//    val isValid = isValidNumber(7, { it > 5 })
+    val isValid = isValidNumber(7, { number -> number > 5 })
+    println(isValid)
+
+    println("---3 es mayor que 5---")
+//    val isValidv2 = isValidNumber(3, { it > 5 })
+    val isValidv2 = isValidNumber(3, { number -> number > 5 })
+    println(isValidv2)
+
+    println("---150 es mayor que 100---")
+    val majorThat100 = { number: Int -> number > 100 }
+    val isValidv3 = isValidNumber(150, majorThat100)
+    println(isValidv3)
+}
+
+//Funcion de orden superior (configurable/dinamico/generico)
+private fun isValidNumber(number: Int, validate: (x: Int) -> Boolean): Boolean {
+    return validate(number)
+}
