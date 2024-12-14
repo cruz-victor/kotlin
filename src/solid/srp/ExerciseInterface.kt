b118c6d57c0eb346c3c57ca67636df0a71c85a94
Victor CG <ubicuosoft@gmail.com>
2024-12-14 01:27:53 -0400

    Interface exercise

--- src/solid/srp/ExerciseInterface.kt
+++ src/solid/srp/ExerciseInterface.kt
@@ -1,2 +1,24 @@
 package solid.srp
 
+interface Fly {
+    fun flying()
+}
+
+class Penguin() : Fly {
+    override fun flying() {
+        println("La ave esta volando")
+    }
+}
+
+class Airplane(): Fly{
+    override fun flying() {
+        println("El avion esta volando")
+    }
+}
+
+fun main(){
+    val flyList= listOf(Penguin(), Airplane())
+    for (flyItem in flyList){
+        flyItem.flying()
+    }
+}
