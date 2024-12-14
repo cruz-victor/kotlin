8b1ed9e08b67d8c0c1aa2746c2ef44b8b7330f7b
Victor CG <ubicuosoft@gmail.com>
2024-12-14 01:05:23 -0400

    Inheritance exercise

--- src/solid/srp/ExerciseInheritance.kt
+++ src/solid/srp/ExerciseInheritance.kt
@@ -1,2 +1,37 @@
 package solid.srp
 
+open class Animal(val name: String) {
+    open fun makeSound() {
+        println("$name esta haciendo sonido.")
+    }
+}
+
+class Dog(name: String) : Animal(name) {
+    override fun makeSound(){
+        println("$name esta ladrando.")
+    }
+}
+
+class Cat(name: String): Animal(name){
+    override fun makeSound(){
+        println("$name esta maullando.")
+    }
+}
+
+class Bird(name: String):Animal(name){
+    override fun makeSound(){
+        println("$name esta kikiriquiando.")
+    }
+}
+
+fun main(){
+    val perro=Dog("Bunker")
+    perro.makeSound()
+
+    val gato=Cat("Pelusa")
+    gato.makeSound()
+
+    val pajaroLoco=Bird("Carmen")
+    pajaroLoco.makeSound()
+}
+
